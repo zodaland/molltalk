d93abe2a423303beb10900d054ed88c96d5e2af2
zodaland <me@zodaland.com>
2022-04-01 02:02:59 +0900

    api 디렉토리명 변경

--- api/app.js
+++ api/app.js
@@ -27,12 +27,8 @@ app.use(express.json())
 //쿠키 파싱
 app.use(cookieParser())
 
-
-//jwt config 임포트
-const jwtConfig = require('./config/jwt_config')
-app.set('jwt-secret', jwtConfig.secretKey)
 //api 라우터 임포트
-app.use('/', require('./api'))
+app.use('/', require('./controller'))
 
 app.use((req, res, next) => {
 	res.status(404).send('잘못된 접근입니다.')
