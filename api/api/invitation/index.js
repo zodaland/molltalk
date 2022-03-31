7ab041330986bf9956c04ab56d72b32c0af1d914
zodaland <me@zodaland.com>
2022-04-01 01:58:13 +0900

    api 디렉토리명 변경

--- api/api/invitation/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router()
-const controller = require('./controller')
-
-router.post('/', controller.create)
-router.get('/to/:no', controller.findByInvitedUser)
-router.get('/from/:no', controller.findByInviteUser)
-router.delete('/:no', controller.delete)
-
-module.exports = router
--- api/api/room/user/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router()
-const controller = require('./controller')
-
-router.get('/user', controller.findRooms)
-router.post('/user', controller.create)
-router.delete('/:no/user', controller.delete)
-
-
-module.exports = router
