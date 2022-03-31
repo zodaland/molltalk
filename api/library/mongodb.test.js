9c08d707819be125ffe462a3884267bce7e95399
zodaland <me@zodaland.com>
2022-04-01 02:03:33 +0900

    mongodb 연결 실패 테스트코드 추가

--- api/library/mongodb.test.js
+++ api/library/mongodb.test.js
@@ -1,15 +1,34 @@
-const mongoose = require('./mongodb');
+const mongoose = require('mongoose');
+const mongodb = require('./mongodb');
+
+jest.mock('mongoose');
 
 describe('mongo test', () => {
     test('mongo connect test', async () => {
-        const isConnect = await mongoose.connect();
+        mongoose.connect.mockReturnValue(Promise.resolve());
+        const isConnect = await mongodb.connect();
 
         expect(isConnect).toBeTruthy();
     });
+
+    test('mongo 연결 실패', async () => {
+        mongoose.connect.mockReturnValue(Promise.reject());
+        const isConnect = await mongodb.connect();
+
+        expect(isConnect).toBeFalsy();
+    });
     
     test('mongo close test', async () => {
-        const isClose = await mongoose.close();
+        mongoose.connection = { close: () => Promise.resolve() };
+        const isClose = await mongodb.close();
 
         expect(isClose).toBeTruthy();
+    });
+
+    test('mongo 해제 실패', async () => {
+        mongoose.connection = { close: () => Promise.reject() };
+        const isClose = await mongodb.close();
+
+        expect(isClose).toBeFalsy();
     });
 });
