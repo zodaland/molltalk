d680770405841cbabf38eee8220361686c7175b3
zodaland <me@zodaland.com>
2022-04-05 18:21:40 +0900

    들여쓰기 스페이스바로 변경, 버전키 비활성화, find 함수 역순으로 정렬하도록 수정

--- api/models/chat.js
+++ api/models/chat.js
@@ -1,22 +1,23 @@
 const mongoose = require('mongoose')
 
 const chatSchema = new mongoose.Schema({
-	room: { type: Number },
-	name: { type: String },
-	content: { type: String, required: true }
-	},
-	{
-		timestamps: true
-	})
+    room: { type: Number },
+    name: { type: String },
+    content: { type: String, required: true }
+    },
+    {
+        timestamps: true,
+        versionKey: false,
+    })
 
 chatSchema.statics.create = function(payload) {
-	const chat = new this(payload)
+    const chat = new this(payload)
 
-	return chat.save()
+    return chat.save()
 }
 
-chatSchema.statics.finds = function(roomNo) {
-	return this.find({room: roomNo}, { _id: false, name: true, content: true, room: true, }).sort({_id: -1}).limit(50)
+chatSchema.statics.findsByRoomNo = function(roomNo) {
+    return this.find({room: roomNo}, { _id: false, name: true, content: true, room: true, }).sort({_id: -1}).limit(50).then(result => result.reverse());
 }
 
 module.exports = mongoose.model('Chat', chatSchema)
