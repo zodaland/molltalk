33a9a30fbff1c8ecbdd0fbf329ec25b34434b469
zodaland <me@zodaland.com>
2022-04-05 18:10:34 +0900

    route 변경

--- api/controller/invitation/index.js
+++ api/controller/invitation/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const invitationController = require('./invitationController')
 
 router.post('/', invitationController.create)
-router.get('/to/:no', invitationController.findsByInvitedUser)
+router.get('/', invitationController.findsByInvitedUser)
 router.delete('/:no', invitationController.delete)
 
 module.exports = router
