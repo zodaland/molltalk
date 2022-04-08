7fba86dc36b3ada0aedbd23576e62e1b205d96bb
zodaland <me@zodaland.com>
2022-04-09 03:31:59 +0900

    세미콜론 추가 작업 및 초대장 거절 함수 추가

--- web/src/services/Invitation.js
+++ web/src/services/Invitation.js
@@ -1,15 +1,22 @@
-import axios from '../library/axios'
+import axios from '../library/axios';
 
 export function create(params) {
 	return axios.post('/invitation', params)
 	.then((response) => {
-		return response
-	})
-}
+		return response;
+	});
+};
 
 export function getMyInvitation(params) {
-	return axios.get(`/invitation/`)
+	return axios.get('/invitation/')
 	.then((response) => {
 		return response;
-	})
+	});
+};
+
+export function del(no) {
+    return axios.delete(`/invitation/${no}`)
+    .then((response) => {
+        return response;
+    });
 };
