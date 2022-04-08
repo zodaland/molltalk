1992ee47ecf431f1581e8c30c5030b69f9f4eb1b
zodaland <me@zodaland.com>
2022-04-09 03:25:58 +0900

    아이디, 타입 스키마에 추가, 조회시 값 포함되도록 수정

--- api/models/chat.js
+++ api/models/chat.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose')
 const chatSchema = new mongoose.Schema({
     room: { type: Number, required: true, },
     name: { type: String, required: true, },
-    content: { type: String }
+    id: { type: String, required: true, },
+    content: { type: String },
+    type: { type: String },
     },
     {
         timestamps: true,
@@ -17,7 +19,7 @@ chatSchema.statics.create = function(payload) {
 }
 
 chatSchema.statics.findsByRoomNo = function(roomNo) {
-    return this.find({room: roomNo}, { _id: false, name: true, content: true, room: true, }).sort({_id: -1}).limit(50).then(result => result.reverse());
+    return this.find({room: roomNo}, { _id: false, id: true, name: true, content: true, type: true, }).sort({_id: -1}).limit(50).then(result => result.reverse());
 }
 
 module.exports = mongoose.model('Chat', chatSchema)
