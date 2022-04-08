4424bdf6b30c5bec97fbcad167005e19392981c5
zodaland <me@zodaland.com>
2022-04-09 03:37:10 +0900

    웹소켓 메시지 전송 모듈 적용, css 적용, 초대 거절 기능 추가, 일부 로직 변경

--- web/src/components/Alarm.js
+++ web/src/components/Alarm.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 
 import { invitedWsMsgState } from '../modules/wsMsg';
 import { userState } from '../modules/user';
@@ -9,24 +9,37 @@ import * as Invitation from '../services/Invitation';
 import * as RoomUser from '../services/RoomUser';
 import * as Room from '../services/Room';
 
+import { WebSocketContext } from '../library/WebSocketProvider';
+
 const Alarm = () => {
     const [alarms, setAlarms] = useState([]);
     const invitedWsMsg = useRecoilValue(invitedWsMsgState);
     const { user } = useRecoilValue(userState);
     const setRooms = useSetRecoilState(roomInfoState);
+    const wsService = useContext(WebSocketContext);
     
     const handleJoin = async (no) => {
         try {
             const joinRes = await RoomUser.create({ no });
-            if (joinRes.status !== 201) {
-                throw new Error();
-            }
+            if (joinRes.status !== 201) throw new Error();
+
             const res = await Room.find();
             if (res.status !== 200) throw new Error();
             setRooms(res.data);
             setAlarms(alarms.filter(alarm => alarm.no!== no));
+
+            if (wsService) wsService.roomEnter(no);
         } catch (error) {
             alert('방 참여 실패');
+        }
+    };
+    const handleDelete = async (no) => {
+        try {
+            const delRes = await Invitation.del(no);
+            if (delRes.status !== 200) throw new Error();
+            setAlarms(alarms.filter(alarm => alarm.no !== no));
+        } catch (error) {
+            alert('초대장 삭제 실패');
         }
     };
 
@@ -53,9 +66,26 @@ const Alarm = () => {
     }, []);
 
     return (
-        <div>
+        <div className="grid lg:grid-cols-3 grid-cols-2">
         {alarms.map(alarm => (
-            <button onClick={() => handleJoin(alarm.no)}>{alarm.id}의 초대 {alarm.name}방 입장</button>
+            <div className="m-2 border border-gray-500 rounded-md" key={alarm.no}>
+                <button
+                    className="w-4/5 h-12 text-sm rounded-l-md hover:bg-green-200 transition"
+                    onClick={() => handleJoin(alarm.no)}
+                >
+                <p className="inline-block w-1/2 h-4 whitespace-nowrap truncate px-1">{alarm.id}</p>
+                님의 초대장
+                <br/>
+                <p className="inline-block w-1/2 h-4 whitespace-nowrap truncate px-1">{alarm.name}</p>
+                방 입장
+                </button>
+                <button
+                    className="float-right w-1/5 h-12 bg-red-200 rounded-r-md hover:bg-red-300 transition"
+                    onClick={() => handleDelete(alarm.no)}
+                >
+                    거절
+                </button>
+            </div>
         ))}
         </div>
     );
