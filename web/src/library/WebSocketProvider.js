6e585739d92f6558de6f85fb20f5a765de24d43a
zodaland <me@zodaland.com>
2022-04-11 11:18:49 +0900

    dependency warning 수정

--- web/src/library/WebSocketProvider.js
+++ web/src/library/WebSocketProvider.js
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect, createContext } from 'react';
+import { useState, useRef, useEffect, useCallback, createContext } from 'react';
 
 import { wsMsgState } from '../modules/wsMsg';
 import { userState } from '../modules/user';
@@ -16,39 +16,46 @@ const WebSocketProvider = (props) => {
     const [wsService, setWsService] = useState(new WsService(ws.current));
 
 	//websocket start
-	const webSocketInit = () => {
+	const webSocketInit = useCallback(() => {
+        const checkConnection = () => {
+            if (!ws.current || ws.current.readyState === WebSocket.CLOSED) {
+                webSocketInit();
+                setTimeout(checkConnection, 500);
+            }
+        };
+
+        const heartbeat = () => {
+            if (ws.current && ws.current.readyState === WebSocket.OPEN) {
+                ws.current.send(JSON.stringify({ type: 'HEART' }));
+                setTimeout(heartbeat, 500);
+            }
+        };
+
 		const webSocketUrl = `wss://api.zodaland.com`;
 
-			ws.current = new WebSocket(webSocketUrl);
-            setWsService(new WsService(ws.current));
-			ws.current.onopen = () => heartbeat();
-			ws.current.onmessage = (evt) => {
-				const data = JSON.parse(evt.data);
-                setWsMsgState(data);
-			}
-			ws.current.onclose = error => isLogin && checkConnection();
-			ws.current.onerror = error => isLogin && checkConnection();
-	}
-
-	//websocket restart
-	const checkConnection = () => {
-		if (!ws.current || ws.current.readyState === WebSocket.CLOSED) {
-			webSocketInit();
-            setTimeout(checkConnection, 500);
-		}
-	};
-    const heartbeat = () => {
-        if (ws.current && ws.current.readyState === WebSocket.OPEN) {
-            ws.current.send(JSON.stringify({ type: 'HEART' }));
-            setTimeout(heartbeat, 500);
+        ws.current = new WebSocket(webSocketUrl);
+        setWsService(new WsService(ws.current));
+        ws.current.onopen = () => heartbeat();
+        ws.current.onmessage = (evt) => {
+            const data = JSON.parse(evt.data);
+            setWsMsgState(data);
         }
-    };
+        ws.current.onclose = error => isLogin && checkConnection();
+        ws.current.onerror = error => isLogin && checkConnection();
+	}, [isLogin, setWsMsgState]);
 
 	//Component Did Mount
 	useEffect(() => {
+        const checkConnection = () => {
+            if (!ws.current || ws.current.readyState === WebSocket.CLOSED) {
+                webSocketInit();
+                setTimeout(checkConnection, 500);
+            }
+        }
+
         if (!isLogin) return;
         checkConnection();
-	}, [isLogin]);
+	}, [isLogin, webSocketInit]);
 
 	return (
 		<WebSocketContext.Provider value={wsService}>
